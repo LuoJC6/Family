928d1b624f090ea68697b0ee3d4dc0eb2bb49c33
18487263092 <1667657502@qq.com>
2016-10-08 09:16:52 +0800

    10

--- gen/com/Family/familyfinances/activity/R.java
+++ gen/com/Family/familyfinances/activity/R.java
@@ -95,7 +95,7 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int family_add=0x7f030003;
         public static final int family_flag=0x7f030004;
-        public static final int family_in=0x7f030005;
+        public static final int family_info=0x7f030005;
         public static final int login=0x7f030006;
     }
     public static final class menu {
