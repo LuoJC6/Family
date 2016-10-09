dcf7a36d711473c5867c5865421843a4ae4cda6b
18487263092 <1667657502@qq.com>
2016-10-09 10:10:33 +0800

    10.99

---
+++ src/com/Family/familyfinances/activity/FirstActivity.java
@@ -0,0 +1,25 @@
+package com.Family.familyfinances.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends Activity {
+	Button btnXinZ;
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geryy);
+        btnXinZ=(Button)findViewById(R.id.btnXinZZC);
+        btnXinZ.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				Intent intent=new Intent(FirstActivity.this,SecoundActivity.class);
+				startActivity(intent);
+			}
+			});
+        }
+}
---
+++ src/com/Family/familyfinances/activity/SecoundActivity.java
@@ -0,0 +1,13 @@
+package com.Family.familyfinances.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecoundActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.family_flag);
+	}
+}
