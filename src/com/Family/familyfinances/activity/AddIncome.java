ce255b03d46974d5458380d416d269efed653306
18487263092 <1667657502@qq.com>
2016-10-12 12:10:22 +0800

    aaa

--- src/com/Family/familyfinances/activity/AddIncome.java
+++ src/com/Family/familyfinances/activity/AddIncome.java
@@ -1,7 +1,6 @@
 package com.Family.familyfinances.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,47 +11,39 @@ import android.widget.Toast;
 import com.Family.familyfinances.dao.InfamilyDAO;
 import com.Family.familyfinances.model.Tb_Infamily;
 
-public class AddIncome extends Activity{
+public class AddIncome extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.family_addincome);
-		//获取控件
-		final EditText txtInMoney;
-		final EditText txtInTime;
-		final EditText txtInHandler, txtInMark;
-		final EditText spInType;
-		final Button btnSave,btnCancel;
-		 txtInMoney = (EditText)findViewById(R.id.txtInMoney);
-		 txtInTime = (EditText)findViewById(R.id.txtInTime);
-		 txtInHandler = (EditText)findViewById(R.id.txtInHandler);
-		 txtInMark = (EditText)findViewById(R.id.txtInMark);
-		 spInType =(EditText)findViewById(R.id.spInType);
-		btnSave = (Button)findViewById(R.id.btnSave);
-		btnCancel = (Button)findViewById(R.id.btnCancel);
-		//保存按钮单击事件
-		btnSave.setOnClickListener(new OnClickListener() {
+		final EditText txtmoney = (EditText)findViewById(R.id.txtInMoney);
+		final EditText txttime = (EditText)findViewById(R.id.txtInTime);
+		final EditText txttype = (EditText)findViewById(R.id.spInType);
+		final EditText txtaddress = (EditText)findViewById(R.id.txtInHandler);
+		final EditText txtmark = (EditText)findViewById(R.id.txtInMark);
+		Button btnsave = (Button)findViewById(R.id.btnSave);
+		Button btnExit = (Button)findViewById(R.id.btnCancel);
+		btnsave.setOnClickListener(new OnClickListener() {
 			
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				InfamilyDAO infamilyDAO = new InfamilyDAO(AddIncome.this);// 创建PwdDAO对象
-				Tb_Infamily tb_infamily=new Tb_Infamily(infamilyDAO.getMaxId() + 1,Double.valueOf(txtInMoney.getText().toString()),
-							txtInTime.getText().toString(),
-							txtInHandler.getText().toString(),
-							txtInMark.getText().toString(),
-							spInType.getText().toString());// 根据输入的密码创建Tb_pwd对象
+				InfamilyDAO infamilyDAO = new InfamilyDAO(AddIncome.this);
+				Tb_Infamily tb_infamily = new Tb_Infamily(infamilyDAO.getMaxId() + 1,
+						Double.valueOf(txtmoney.getText().toString()),
+						txttime.getText().toString(),
+						txttype.getText().toString(),
+						txtaddress.getText().toString(),
+						txtmark.getText().toString());
 				
-				infamilyDAO.add(tb_infamily);// 添加收入信息
-				//提示信息
+				infamilyDAO.add(tb_infamily);
 				Toast.makeText(AddIncome.this, "新增成功", Toast.LENGTH_SHORT)
-				.show();
+						.show();
 				finish();
 			}
 		});
-		//取消按钮单击事件
-		btnCancel.setOnClickListener(new OnClickListener() {
+		btnExit.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
@@ -61,6 +52,4 @@ public class AddIncome extends Activity{
 			}
 		});
 	}
-
-
 }
--- src/com/Family/familyfinances/activity/NewPay.java
+++ src/com/Family/familyfinances/activity/NewPay.java
@@ -17,32 +17,37 @@ public class NewPay extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.family_newpay);
-		//����ؼ�
 		final EditText txtmoney = (EditText)findViewById(R.id.txtmoney);
 		final EditText txttime = (EditText)findViewById(R.id.txttime);
 		final EditText txttype = (EditText)findViewById(R.id.txttype);
 		final EditText txtaddress = (EditText)findViewById(R.id.txtaddress);
 		final EditText txtmark = (EditText)findViewById(R.id.txtmark);
 		Button btnsave = (Button)findViewById(R.id.btnpaySave);
-		//��ť����¼�
+		Button btnexit = (Button)findViewById(R.id.btnCancel);
 		btnsave.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				OutfamilyDAO outfamilyDAO = new OutfamilyDAO(NewPay.this);// ����PwdDAO����
+				OutfamilyDAO outfamilyDAO = new OutfamilyDAO(NewPay.this);
 				Tb_Outfamily tb_outfamily = new Tb_Outfamily(outfamilyDAO.getMaxId() + 1,
 						Double.valueOf(txtmoney.getText().toString()),
 						txttime.getText().toString(),
 						txttype.getText().toString(),
 						txtaddress.getText().toString(),
-						txtmark.getText().toString());// ������������봴��Tb_pwd����
+						txtmark.getText().toString());
 				
-				outfamilyDAO.add(tb_outfamily);// ����û�����
-				 
-				// ������Ϣ��ʾ
+				outfamilyDAO.add(tb_outfamily);
 				Toast.makeText(NewPay.this, "新增成功", Toast.LENGTH_SHORT)
 						.show();
+				finish();
+			}
+		});
+		btnexit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
 				finish();
 			}
 		});
